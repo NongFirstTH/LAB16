35757b062d8b9ba6688e692065f49fd66a0b8381
NongFirstTH <TanabodeeChannel@gmail.com>
2023-02-07 16:23:51 +0700

    OK

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,22 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+int x[]={a,b,c,d};
+int ans[4];
+int i=0;
+int R[4]={1,1,1,1};
+while(i<4){
+    int r = rand()%4;
+        if(R[r]==1){
+    ans[i] = x[r];
+    R[r]=0;
+    i++;
+        }
+    }
+a = ans[0];
+b = ans[1];
+c= ans[2];
+d= ans[3];
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,28 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+int x[]={*a,*b,*c,*d};
+int ans[4];
+int i=0;
+int R[4]={1,1,1,1};
+while(i<4){
+    int r = rand()%4;
+        if(R[r]==1){
+    ans[i] = x[r];
+    R[r]=0;
+    i++;
+        }
+    }
+*a = ans[0];
+*b = ans[1];
+*c= ans[2];
+*d= ans[3];
+}
